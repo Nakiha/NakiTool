4b7d0669119c9f58acd74e43a225da0db74f978c
Naki <naki2642@gmail.com>
2025-03-20 01:15:56 +0800

    fix debug error && upgrade deps

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import electron from 'vite-plugin-electron/simple'
 import pkg from './package.json'
+import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => {
@@ -61,6 +62,11 @@ export default defineConfig(({ command }) => {
         // See 👉 https://github.com/electron-vite/vite-plugin-electron-renderer
         renderer: {},
       }),
+      visualizer({
+        emitFile: false,
+        filename: 'dist-electron/report.html',
+        open: true,
+      })
     ],
     server: process.env.VSCODE_DEBUG && (() => {
       const url = new URL(pkg.debug.env.VITE_DEV_SERVER_URL)
